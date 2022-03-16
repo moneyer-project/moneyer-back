5f4ab17ad217eb0dea8f503bc89c1b4287ead5e6
Gwennael Jean <gwennael.jean@digiwin.fr>
2022-03-16 08:40:19 +0100

    feat(dashboard): affichage des revenus et des dépenses

--- assets/controllers/dashboard_controller.js
+++ assets/controllers/dashboard_controller.js
@@ -3,35 +3,50 @@ import moneyFormatter from '../js/services/money-formatter';
 
 export default class extends Controller {
 
-    static targets = ['account'];
+    static targets = ['account', 'externalAccount'];
 
     static values = {
         url: String
     };
 
     connect() {
-        this.load();
+        this.loadAccounts();
     }
 
     load(e) {
-        this.loadAccount(e);
+        this.loadAccounts(e);
     }
 
-    loadAccount(e) {
-        let url = e === undefined
-            ? this.urlValue
-            : this.urlValue + '?' + new URLSearchParams({
-                month: e.detail.month,
-                year: e.detail.year
-            });
+    loadAccounts(e) {
+        let url = e === undefined ? this.urlValue : this.urlValue + '?' + new URLSearchParams({
+            month: e.detail.month,
+            year: e.detail.year
+        });
 
         fetch(url)
             .then(response => response.text())
             .then(text => JSON.parse(text))
             .then(json => {
-                if (this.hasAccountTarget) {
-                    this.accountTarget.textContent = moneyFormatter.format(json.account.balance);
-                }
+                this.feedAccount(json.account);
+                this.feedExternalAccounts(Object.values(json.externalAccounts));
             });
+    }
+
+    feedAccount(account) {
+        if (this.hasAccountTarget) {
+            this.accountTarget.setAttribute('data-account-entity-value', JSON.stringify(account));
+        }
+    }
+
+    feedExternalAccounts(accounts) {
+        this.externalAccountTargets.forEach((element) => {
+            let account = accounts.find(account => {
+                return account.id === parseInt(element.dataset.id);
+            });
+
+            if (account !== undefined) {
+                element.setAttribute('data-account-entity-value', JSON.stringify(account));
+            }
+        })
     }
 }
--- src/Controller/Api/AccountController.php
+++ src/Controller/Api/AccountController.php
@@ -2,8 +2,11 @@
 
 namespace App\Controller\Api;
 
+use App\Entity\Bank\Account;
 use App\Entity\User;
 use App\Helper\DateTimeHelper;
+use App\Repository\Bank\AccountRepository;
+use Doctrine\Common\Collections\ArrayCollection;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
@@ -14,19 +17,27 @@ use Symfony\Component\Serializer\SerializerInterface;
 class AccountController extends AbstractController
 {
     #[Route('/api/account', name: 'app_api_account')]
-    public function index(Request $request, SerializerInterface $serializer): Response
+    public function index(Request $request, SerializerInterface $serializer, AccountRepository $accountRepository): Response
     {
         if (!$this->getUser() instanceof User) {
             throw $this->createAccessDeniedException();
         }
 
-        return $this->json([
-            'account' => $serializer->normalize($this->getUser()->getAccount(), null, [
-                'date' => DateTimeHelper::getByRequest($request),
-                AbstractNormalizer::ATTRIBUTES => [
-                    'name',
-                ],
-            ])
-        ]);
+        $accounts = new ArrayCollection($accountRepository->findByUser($this->getUser()));
+
+        $account = $accounts->filter(fn (Account $account) => $account->getUser() === $this->getUser())->first();
+        $externalAccounts = $accounts->filter(fn (Account $account) => $account->getUser() !== $this->getUser());
+
+
+
+        return $this->json($serializer->normalize([
+            'account' => $account,
+            'externalAccounts' => $externalAccounts,
+        ], null, [
+            'date' => DateTimeHelper::getByRequest($request),
+            AbstractNormalizer::GROUPS => [
+                'account:default',
+            ],
+        ]));
     }
 }
