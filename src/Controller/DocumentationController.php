3afcf021c155838fc078da53cb97825764231de0
Gwennael Jean <gwennael.jean@gmail.com>
2022-03-30 09:49:31 +0200

    feat(doc): création d'une page listant les icones de l'application

---
+++ src/Controller/DocumentationController.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace App\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+
+class DocumentationController extends AbstractController
+{
+    #[Route('/documentation', name: 'app_documentation')]
+    public function index(): Response
+    {
+        return $this->render('documentation/index.html.twig', [
+            'controller_name' => 'DocumentationController',
+        ]);
+    }
+}
