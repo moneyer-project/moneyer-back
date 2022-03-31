d677f52f0ab5a40b286f9487ef2feaa76e4a3d0c
Gwennael Jean <gwennael.jean@gmail.com>
2022-03-31 22:03:48 +0200

    feat(charge): chargement en ajax du contenu de la modal

--- src/Controller/ChargeController.php
+++ src/Controller/ChargeController.php
@@ -4,16 +4,16 @@ namespace App\Controller;
 
 use App\Entity\Bank\Account;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
 class ChargeController extends AbstractController
 {
     #[Route('/account/{account}/charges', name: 'app_account_charges')]
-    public function index(Account $account): Response
+    public function index(Request $request, Account $account): Response
     {
         return $this->render('charge/index.html.twig', [
-            'controller_name' => 'ChargeController',
             'account' => $account,
         ]);
     }
